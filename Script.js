636382be3792032a07ec3a81d649bd63cb5f891d
Shea Hucock <191794597+Swissly@users.noreply.github.com>
2024-12-30 06:00:54 +1000

    Website overhaul

--- Script.js
+++ Script.js
@@ -1,2 +1,6 @@
 // JavaScript source code
 console.log("Welcome to my portfolio website!");
+document.querySelectorAll('.parallax').forEach(section => {
+    const image = section.getAttribute('data-image');
+    section.style.backgroundImage = `url(${image})`;
+  });
